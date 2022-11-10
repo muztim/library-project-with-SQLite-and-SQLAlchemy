abc15fb6b7efdc8311ecf71bbd8a777c2d8566e0
Timothee Muzaliwa <tmuzaliwwa@gmail.com>
2022-11-10 19:38:10 +0300

    -get new book data -print all_books to home page if any -connect to database

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
 
 app = Flask(__name__)
+db = sqlite3.connect('books-collection.bd')
+cursor = db.cursor()
+cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
+               "NOT NULL, rating FLOAT NOT NULL) ")
+
 
 all_books = []
+books = []
+data_file = {}
 
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['POST', 'GET'])
 def add():
-    pass
+    if request.method == 'POST':
+        all_books.append(request.form.to_dict())
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
