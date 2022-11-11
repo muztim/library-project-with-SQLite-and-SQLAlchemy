9f535127c5274aae723329af80efb2350346666e
Timothee Muzaliwa <tmuzaliwwa@gmail.com>
2022-11-11 20:52:12 +0300

    Add rating edit and delete book functionalities

--- main.py
+++ main.py
@@ -14,20 +14,14 @@ db = SQLAlchemy(app=app)
 class NewBook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.String(250), nullable=False)
 
     def __repr__(self):
         return f'<Book {self.title}>'
 
 
-db.create_all()
-
-# -------------- Create records ----------- #
-newbook = NewBook(title="Harry Potter", author="J. K. Rowling", rating=9.1)
-db.session.add(newbook)
-db.session.commit()
-
+db.create_all()           # create a database
 
 # db = sqlite3.connect('')
 # cursor = db.cursor()
@@ -38,9 +32,34 @@ db.session.commit()
 # db.commit()
 
 
+# -------------- Create records ----------- #
+# newbook = NewBook(title="Deep Work", author="C. Newport", rating=8.5)
+# db.session.add(newbook)
+# db.session.commit()
+
+# -------------- CRUD records with SQLAlchemy ---------------#
+
+# book_to_update = NewBook.query.filter_by(title="Harry Potter").first()   # how to read record
+# # print(book_to_update)
+# book_to_update.title = "Harry Potter and the Chamber of Secrets"     # update a record by query
+# db.session.commit()
+
+# book_id = 1
+# book_to_update = NewBook.query.get(book_id)
+# book_to_update.title = "Harry Potter and the Goblet of Fire"    # update a record by primary key
+# db.session.commit()
+
+
+# book_id = 10
+# book_to_delete = NewBook.query.get(book_id)
+# db.session.delete(book_to_delete)
+# db.session.commit()
+
+
 @app.route('/')
 def home():
     all_books = db.session.query(NewBook).all()
+    print(all_books)
     return render_template('index.html', books=all_books)
 
 
@@ -52,6 +71,29 @@ def add():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
+
+
+@app.route("/edit", methods=['POST', 'GET'])
+def edit():
+    if request.method == 'POST':
+        # update rating record
+        book_id = request.form["id"]
+        book_to_update = NewBook.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = NewBook.query.get(book_id)
+    return render_template('edit_rating.html', book=book_selected)
+
+
+@app.route('/delete')
+def delete():
+    book_id = request.args.get('id')
+    # delete a record by ID
+    db.session.delete(book_id)
+    db.session.commit()
+    return render_template(url_for('home'))
 
 
 if __name__ == "__main__":
